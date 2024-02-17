6ed64dcf01118910cc938640e4a2cebf2f1dbd4e
TUF <nguyenvo1373.hg@gmail.com>
2024-02-17 16:43:12 +0700

    19 done

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+// use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -20,6 +22,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'avatar',
         'password',
     ];
 
@@ -42,4 +45,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function name():Attribute{
+        return Attribute::make(
+            get: fn ($value) => strtoupper($value)
+        );
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,79 @@
 <?php
 
+use App\Http\Controllers\Profile\AvatarController;
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); 
+
+    // Query simple
+
+    // fetch all users
+    // $users = DB::select("select * from users");
+
+    // create user
+    // $user = DB::insert("insert into users (name, email, password) value (?, ?, ?)",
+    //     ['Kiet 1', 
+    //     'kiet1@gmail.com', 
+    //     '123456']
+    // );
+
+    // update user
+    // $user = DB::update("update users set email =? where id =? ", [
+    //     'kiet@gmail.com', 2,
+    // ]);
+
+    //delete user
+    // $user = DB::delete("delete from users where id=2") ;
+
+    // Query builder
+
+    // fetch user
+    // $users = DB::table('users')->get() ;
+    // $users = DB::table('users')->find(7) ;
+
+    // insert user
+    // $user = DB::table('users')->insert([
+    //     'name' => 'Kiet 2',
+    //     'email' => 'kiet2@gmail.com',
+    //     'password' => '123456',
+    // ]);
+
+    // update user
+    // $user = DB::table('users')->where('id', 5)->update([
+    //     'email' => 'abc@gmail.com'
+    // ]);
+
+    // delete user
+    // $user = DB::table('users')->where('id', 7)->delete();
+
+
+    // eloquent ORM
+    // $users = User::where('id',10)->first();
+    // $users = User::find(15);
+
+    // create user
+    // $user = User::create([
+    //     'name' => 'Kiet',
+    //     'email' => 'kiet123@gmail.com',
+    //     'password' => '123456',
+    // ]);
+
+    // update user
+    // $user = User::where('id', 10)->update([
+    //     'email' => 'abc@gmail.com'
+    // ]);
+
+    // delete user
+    // $user = User::find(10);
+    // $user->delete();
+    
+    // dd($users->name);
 });
 
 Route::get('/dashboard', function () {
@@ -25,6 +83,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
